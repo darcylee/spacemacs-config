243975c021339627a69d3004aec7a18e7b0d44a5
lijq <lijq@newlandcomputer.com>
2016-09-30 17:46:41 +0800

    make "s-x" keybindings to global setting

--- layers/darcylee/keybindings.el
+++ layers/darcylee/keybindings.el
@@ -9,15 +9,14 @@
 ;;
 ;;; License: GPLv3
 
-
-(when (spacemacs/system-is-mswindows)
-  (global-set-key (kbd "s-=") 'spacemacs/scale-up-font)
-  (global-set-key (kbd "s--") 'spacemacs/scale-down-font)
-  (global-set-key (kbd "s-0") 'spacemacs/reset-font-size)
-  (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
-  (global-set-key (kbd "s-v") 'yank)
-  (global-set-key (kbd "s-c") 'evil-yank)
-  (global-set-key (kbd "s-a") 'mark-whole-buffer)
-  (global-set-key (kbd "s-x") 'kill-region)
-  (global-set-key (kbd "s-w") 'delete-window)
-  (global-set-key (kbd "s-W") 'delete-frame))
+(global-set-key (kbd "s-=") 'spacemacs/scale-up-font)
+(global-set-key (kbd "s--") 'spacemacs/scale-down-font)
+(global-set-key (kbd "s-0") 'spacemacs/reset-font-size)
+(global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
+(global-set-key (kbd "s-v") 'yank)
+(global-set-key (kbd "s-c") 'evil-yank)
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
+(global-set-key (kbd "s-x") 'kill-region)
+(global-set-key (kbd "s-w") 'delete-window)
+(global-set-key (kbd "s-W") 'delete-frame)
+(global-set-key (kbd "s-d") 'kill-line)
