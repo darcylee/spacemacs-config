d54abc5713339934c0be9a0cb80c97895f44c815
dl@rMBP <darcylee1986@gmail.com>
2016-10-01 10:10:49 +0800

    解决macOS平台'power-line'边界发虚的问题

--- init.el
+++ init.el
@@ -163,6 +163,8 @@
  )
 
 (defun dotspacemacs/user-config ()
+  ;;for macOS
+  (setq ns-use-srgb-colorspace nil)
   ;;解决org表格里面中英文对齐的问题
   (when (configuration-layer/layer-usedp 'chinese)
     (when (and (spacemacs/system-is-mac) window-system)
