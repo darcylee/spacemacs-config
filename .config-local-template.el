65168f3262ffe9bd8161c109dfbc0ea01f79f3cb
darcylee <darcylee1986@gmail.com>
2018-09-14 14:30:41 +0800

    update .config-local-template for "bug-reference-url-format"

--- .config-local-template.el
+++ .config-local-template.el
@@ -2,6 +2,8 @@
 
 (setq custom-gtags-lib-path (concat "/path1:/path2"))
 
+(setq bug-reference-url-format "http://url=%s")
+
 (setq-default
  user-mail-address "xxx@xxx.xx"
  user-full-name "your name")
