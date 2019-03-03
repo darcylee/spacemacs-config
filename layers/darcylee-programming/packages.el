403bf1a7d12246a041e9f354ab2b0879d452c991
darcylee <darcylee1986@gmail.com>
2019-03-03 21:10:46 +0800

    fix new version spacemacs.

--- layers/darcylee-programming/packages.el
+++ layers/darcylee-programming/packages.el
@@ -482,18 +482,18 @@
 
 
     ;; http://stackoverflow.com/questions/23553881/emacs-indenting-of-c11-lambda-functions-cc-mode
-    (defadvice c-lineup-arglist (around my activate)
-      "Improve indentation of continued C++11 lambda function opened as argument."
-      (setq ad-return-value
-            (if (and (equal major-mode 'c++-mode)
-                     (ignore-errors
-                       (save-excursion
-                         (goto-char (c-langelem-pos langelem))
-                         ;; Detect "[...](" or "[...]{". preceded by "," or "(",
-                         ;;   and with unclosed brace.
-                         (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
-                0                       ; no additional indent
-              ad-do-it)))               ; default behavior
+    ;; (defadvice c-lineup-arglist (around my activate)
+    ;;   "Improve indentation of continued C++11 lambda function opened as argument."
+    ;;   (setq ad-return-value
+    ;;         (if (and (equal major-mode 'c++-mode)
+    ;;                  (ignore-errors
+    ;;                    (save-excursion
+    ;;                      (goto-char (c-langelem-pos langelem))
+    ;;                      ;; Detect "[...](" or "[...]{". preceded by "," or "(",
+    ;;                      ;;   and with unclosed brace.
+    ;;                      (looking-at ".*[(,][ \t]*\\[[^]]*\\][ \t]*[({][^}]*$"))))
+    ;;             0                       ; no additional indent
+    ;;           ad-do-it)))               ; default behavior
 
     (with-eval-after-load 'c++-mode
       (define-key c++-mode-map (kbd "s-.") 'company-ycmd)))
