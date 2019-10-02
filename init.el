4657a34ef6cfe56f31c67c23a48db80ddb26c94c
darcylee <darcylee1986@gmail.com>
2019-10-02 22:47:51 +0800

    Remove treemacs-magit package

--- init.el
+++ init.el
@@ -165,7 +165,7 @@ This function should only modify configuration layer settings."
                     helm-c-yasnippet ace-jump-helm-line helm-make magithub
                     helm-spacemacs-help smeargle helm-themes
                     ido-vertical-mode flx-ido company-quickhelp counsel-projectile
-                    ivy-purpose helm-purpose neotree treemacs-projectile treemacs-evil treemacs
+                    ivy-purpose helm-purpose neotree treemacs-projectile treemacs-evil treemacs treemacs-magit
                     helm-rtags flycheck-rtags company-rtags rtags
                     company-ycm flycheck-ycmd company-ycmd
                     spaceline-all-the-icons all-the-icons memoize font-lock+
