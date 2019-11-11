426e666718cf07c766196aab0228cd923b7c16af
darcylee <darcylee1986@gmail.com>
2019-11-11 15:31:47 +0800

    Tweak ivy kbd again

--- layers/zilongshanren-misc/packages.el
+++ layers/zilongshanren-misc/packages.el
@@ -829,7 +829,7 @@
     (define-key ivy-minibuffer-map (kbd "s-o") 'ivy-dispatching-done)
     (define-key ivy-minibuffer-map (kbd "C-c C-e") 'spacemacs//counsel-edit)
     (define-key ivy-minibuffer-map (kbd "<f3>") 'ivy-occur)
-    (define-key ivy-minibuffer-map (kbd "TAB") 'ivy-call)
+    (define-key ivy-minibuffer-map (kbd "TAB") 'ivy-call-and-recenter)
     (define-key ivy-minibuffer-map (kbd "C-s-j") 'ivy-immediate-done)
     ;; (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
     ;; (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
